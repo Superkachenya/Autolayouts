0b57493fc04a38fca838eb0ec14bea44e1f9cc67
Danil <voitenko.cr@gmail.com>
2016-02-09 17:01:24 +0200

    Create new project;

---
+++ Autolayouts-iOS/Autolayouts-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Autolayouts-iOS
+//
+//  Created by Danil on 09.02.16.
+//  Copyright © 2016 Cleveroad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
