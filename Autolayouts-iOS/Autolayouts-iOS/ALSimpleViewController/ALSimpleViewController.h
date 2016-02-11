875ae05cb816974b31a36b29848a8077f37213a9
Danil <voitenko.cr@gmail.com>
2016-02-11 15:11:53 +0200

    add new view; add image(circled and with border); add other elements;add contraints;

---
+++ Autolayouts-iOS/Autolayouts-iOS/ALSimpleViewController/ALSimpleViewController.h
@@ -0,0 +1,13 @@
+//
+//  ALSimpleViewController.h
+//  Autolayouts-iOS
+//
+//  Created by Danil on 11.02.16.
+//  Copyright © 2016 Cleveroad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALSimpleViewController : UIViewController
+
+@end
---
+++ Autolayouts-iOS/Autolayouts-iOS/ALSimpleViewController/ALSimpleViewController.m
@@ -0,0 +1,43 @@
+//
+//  ALSimpleViewController.m
+//  Autolayouts-iOS
+//
+//  Created by Danil on 11.02.16.
+//  Copyright © 2016 Cleveroad. All rights reserved.
+//
+
+#import "ALSimpleViewController.h"
+
+@interface ALSimpleViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *userPhoto;
+@property (weak, nonatomic) IBOutlet UILabel *userName;
+
+@end
+
+@implementation ALSimpleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   // self.userPhoto.image = [UIImage imageNamed:@"photo"];
+    self.userPhoto.layer.cornerRadius = self.userPhoto.frame.size.width / 2;
+    self.userPhoto.clipsToBounds = YES;
+    self.userPhoto.layer.borderWidth = 3.0f;
+    self.userPhoto.layer.borderColor = [UIColor orangeColor].CGColor;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
